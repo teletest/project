5139f54b99096c5b96b7c801d53cfe9732e64f67
anbhalli <anbhalli@yahoo.com>
2010-05-16 22:58:02 +0500

    changes in google map

--- system/application/views/projects/google_map.php
+++ system/application/views/projects/google_map.php
@@ -1,10 +1,10 @@
 <?PHP
-$this->load->library('Cigooglemapapi');
+/*$this->load->library('Cigooglemapapi');
 $this->cigooglemapapi->setAPIKey('yourapikey'); 
 
 $this->cigooglemapapi->addMarkerByAddress('621 N 48th St # 6 Lincoln NE 68502','PJ Pizza','<b>PJ Pizza</b>');
 $this->cigooglemapapi->addMarkerByAddress('826 P St Lincoln NE 68502','Old Chicago','<b>Old Chicago</b>');
-$this->cigooglemapapi->addMarkerByAddress('3457 Holdrege St Lincoln NE 68502',"Valentino's","<b>Valentino's</b>");
+$this->cigooglemapapi->addMarkerByAddress('3457 Holdrege St Lincoln NE 68502',"Valentino's","<b>Valentino's</b>"); */
 ?>
     <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
     <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml">
